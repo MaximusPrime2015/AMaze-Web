88671d9eb9aaf7bd74056e0355e3ca8319686345
Max <max.anisimov230@gmail.com>
2016-05-19 19:32:16 +0300

    Created project using examples for presentations.

---
+++ src/java/MainServlet.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Max
+ */
+@WebServlet(urlPatterns = {"/MainServlet"})
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response, "Get");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response, "Post");
+    }
+
+    protected void
+            processRequest(HttpServletRequest request,
+                    HttpServletResponse response, String type)
+            throws ServletException, IOException {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html><head>");
+            out.println("<title>Servlet MyServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>" + type + " request at "
+                    + sdf.format(cal.getTime()) + "</h1>");
+            out.println("</body></html>");
+        }
+    }
+}
